d100e0f0218b5d70ba0c08172f5d6227ccb9b83f
mtsgi <oq@live.jp>
2019-06-06 15:37:35 +0900

    Add Timeline Loading Chain - 1.1

--- githubtl.js
+++ githubtl.js
@@ -1,4 +1,4 @@
-// GitHub TL Viewer v1.0
+// GitHub TL Viewer v1.1
 
 $( ()=>{
     $( "#username" ).keypress( function( e ) {
@@ -13,7 +13,11 @@ function load(){
     $.getJSON("https://api.github.com/users/" +username+ "/received_events", function(data){
         $("#loading").hide();
         for( let i of data ){
-            $("#tlarea").append( "<kit-box><img src='" + i.actor.avatar_url + "'><span class='kit-small'>" + i.created_at + "</span><a href='https://github.com/" + i.repo.name + "'><strong>" + i.actor.login + "</strong> : " + i.type + "</a><div class='kit-small'>" + i.repo.name + "</div></kit-box>" );
+            $("#tlarea").append( "<kit-box><img src='" + i.actor.avatar_url + "'><span class='kit-small'>" + i.created_at + "</span><strong class='loaduser'><a>" + i.actor.login + "</a></strong> : " + i.type + "<div class='kit-small'><a href='https://github.com/" + i.repo.name + "'>" + i.repo.name + "</a></div></kit-box>" );
         }
+        $(".loaduser").on("click", function(){
+            $("#username").val( $(this).text() );
+            load();
+        });
     });
 }
