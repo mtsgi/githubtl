9bc3d2dd08a5c233b364afc3bcfe8ce5e43b50c7
mtsgi <oq@live.jp>
2020-03-20 22:59:33 +0900

    Update UI style

--- githubtl.js
+++ githubtl.js
@@ -1,23 +1,21 @@
-// GitHub TL Viewer v1.1
+// GitHub TL Viewer v2
 
-$( ()=>{
-    $( "#username" ).keypress( function( e ) {
-        if( e.which == 13 ) load();
-    } );
-} );
+$(() => {
+  $("#username").keypress(function(e) { if(e.which == 13) load() });
+});
 
-function load(){
-    $("#loading").show();
-    let username = $("#username").val();
-    $("#tlarea").html("");
-    $.getJSON("https://api.github.com/users/" +username+ "/received_events", function(data){
-        $("#loading").hide();
-        for( let i of data ){
-            $("#tlarea").append( "<kit-box><img src='" + i.actor.avatar_url + "'><span class='kit-small'>" + i.created_at + "</span><strong class='loaduser'><a>" + i.actor.login + "</a></strong> : " + i.type + "<div class='kit-small'><a href='https://github.com/" + i.repo.name + "'>" + i.repo.name + "</a></div></kit-box>" );
-        }
-        $(".loaduser").on("click", function(){
-            $("#username").val( $(this).text() );
-            load();
-        });
+function load() {
+  $("#loading").show();
+  let username = $("#username").val();
+  $("#tlarea").html("");
+  $.getJSON("https://api.github.com/users/" + username + "/received_events", function(data) {
+    $("#loading").hide();
+    for(let i of data) {
+      $("#tlarea").append("<kit-box><img src='" + i.actor.avatar_url + "'><span class='kit-sub'>" + i.created_at + "</span><strong class='loaduser'><a>" + i.actor.login + "</a></strong> : " + i.type + "<div class='kit-small'><a href='https://github.com/" + i.repo.name + "'>" + i.repo.name + "</a></div></kit-box>");
+    }
+    $(".loaduser").on("click", function() {
+      $("#username").val($(this).text());
+      load();
     });
+  });
 }
